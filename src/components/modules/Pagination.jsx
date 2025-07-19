cc803c1e214f8fe0858a8aff1670f42dabaad3cb
Diana Abyaneh <diana.abyaneh@gmail.com>
2025-07-20 00:52:22 +0330

    Refactor Pagination component to use CSS modules for improved styling and layout

--- src/components/modules/Pagination.jsx
+++ src/components/modules/Pagination.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
+import styles from "./Pagination.module.css";
 
-function Pagination({page, setPage}) {
-
+function Pagination({ page, setPage }) {
   const prevHandler = () => {
     if (page <= 1) return;
     setPage((page) => page - 1);
@@ -12,20 +12,22 @@ function Pagination({page, setPage}) {
     setPage((page) => page + 1);
   };
   return (
-    <div>
-      <button onClick={prevHandler}>Previous</button>
-      <p>1</p>
-      <p>2</p>
+    <div className={styles.pagination}>
+      <button onClick={prevHandler} className={page === 1 ? styles.disabled : null}>
+        Previous
+      </button>
+      <p className={page === 1 ? styles.selected : null}>1</p>
+      <p className={page === 2 ? styles.selected : null}>2</p>
       {page > 2 && page < 9 && (
         <>
-        <span>...</span>
-        <p>{page}</p>
+          <span>...</span>
+          <p className={styles.selected}>{page}</p>
         </>
       )}
       <span>...</span>
-      <p>9</p>
-      <p>10</p>
-      <button onClick={nextHandler}>Next</button>
+      <p className={page === 9 ? styles.selected : null}>9</p>
+      <p className={page === 10 ? styles.selected : null}>10</p>
+      <button onClick={nextHandler} className={page === 10 ? styles.disabled : null}>Next</button>
     </div>
   );
 }
